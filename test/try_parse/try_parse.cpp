f258baf70d41c79500c58266387e35dfb0edd2c7
John Turner <jturner.usa@gmail.com>
2024-03-24 15:30:00 -0400

    renamed member from value to result to avoid "result.value.value()"

--- test/try_parse/try_parse.cpp
+++ test/try_parse/try_parse.cpp
@@ -15,17 +15,17 @@ int main(int argc, char **argv) {
     {
         auto category = pms_utils::try_parse(catpkg, pms_utils::parsers::category());
         assert(category.status == pms_utils::ParserStatus::Progress);
-        assert(*category.value == "app-editors");
+        assert(*category.result == "app-editors");
         auto rest = std::string_view(catpkg.begin() + category.consumed + 1, catpkg.end());
         auto name = pms_utils::try_parse(rest, pms_utils::parsers::name());
         assert(name.status == pms_utils::ParserStatus::Success);
-        assert(*name.value == "emacs");
+        assert(*name.result == "emacs");
     }
 
     {
         auto result = pms_utils::try_parse(atom, pms_utils::parsers::category());
         assert(result.status == pms_utils::ParserStatus::Fail);
-        assert(!result.value.has_value());
+        assert(!result.result.has_value());
         assert(result.consumed == 0);
     }
 }
