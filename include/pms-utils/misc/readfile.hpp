5e781f20fd5ba301f4d5b3ee081d2acebd72aafd
John Turner <jturner.usa@gmail.com>
2025-02-18 22:13:53 -0500

    create try_readfile utility function

---
+++ include/pms-utils/misc/readfile.hpp
@@ -0,0 +1,27 @@
+#include <cerrno>
+#include <expected>
+#include <filesystem>
+#include <fstream>
+#include <vector>
+
+namespace pms_utils::misc {
+std::expected<std::vector<char>, int> try_readfile(std::filesystem::path &path) {
+    auto file_size = std::filesystem::file_size(path);
+
+    if (file_size == 0) {
+        return {};
+    }
+
+    std::ifstream fsteam(path, std::ios::binary);
+
+    if (!fsteam.is_open()) {
+        return std::unexpected(errno);
+    }
+
+    std::vector<char> buffer(file_size);
+
+    fsteam.read(buffer.data(), file_size);
+
+    return buffer;
+}
+} // namespace pms_utils::misc
