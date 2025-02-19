7d96916e9febbc90f3bb8014b8f5e7a2f2aea858
John Turner <jturner.usa@gmail.com>
2025-02-18 21:41:00 -0500

    fixup

--- .dir-locals.el
+++ .dir-locals.el
@@ -4,7 +4,7 @@
      (eval . (add-to-list 'eglot-server-programs '(c++-mode . ("clangd" "-header-insertion=never"))))
      (eval . (eglot-ensure))
      (eval . (company-mode 1))))
- (c-ts-mode
+ (c++-ts-mode
   . ((fmt-executable . "clang-format")
      (eval . (add-hook 'before-save-hook 'fmt-current-buffer nil t))
      (eval . (add-to-list 'eglot-server-programs '(c++-mode . ("clangd" "-header-insertion=never"))))
