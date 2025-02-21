fab6c39d1b4b1cc3a2477922052e149a04d2ea8b
John Turner <jturner.usa@gmail.com>
2025-02-21 01:32:45 -0500

    debugging

--- lib/vdb/vdb.cpp
+++ lib/vdb/vdb.cpp
@@ -1,7 +1,11 @@
+#include "pms-utils/depend/depend.hpp"
+#include "pms-utils/depend/depend_parser.hpp"
 #include "pms-utils/misc/readfile.hpp"
 #include "pms-utils/misc/try_parse.hpp"
 #include "pms-utils/vdb/vdb.hpp"
 
+#include <algorithm>
+#include <boost/parser/parser.hpp>
 #include <charconv>
 #include <cstdint>
 #include <cstring>
@@ -18,13 +22,13 @@ namespace {
 struct Error {
 public:
     Error() = default;
+    virtual ~Error();
     virtual void error(std::string) = 0;
 };
 
 struct ParseError : Error {
 public:
     ParseError(std::string unconsumed) : unconsumed(std::move(unconsumed)) {};
-    virtual ~ParseError() = default;
 
     void error(std::string message) override {
         throw std::runtime_error(std::format("parser error: {}", message));
@@ -37,7 +41,6 @@ private:
 struct NotANumber : Error {
 public:
     NotANumber(std::string data) : data(std::move(data)) {};
-    virtual ~NotANumber() = default;
 
     void error(std::string message) override {
         throw std::runtime_error(std::format("not a number: {}", message));
@@ -50,9 +53,10 @@ private:
 struct IoError : Error {
 public:
     IoError(int err) : err(err) {};
-    virtual ~IoError() = default;
 
-    void error(std::string message) override { throw std::runtime_error(std::format("IO error")); };
+    void error(std::string message) override {
+        throw std::runtime_error(std::format("{}: IO error: {}", message, std::strerror(err)));
+    };
 
 private:
     int err;
@@ -70,7 +74,7 @@ template <typename T, typename I> std::expected<I, NotANumber> read_int(T data) 
     return number;
 }
 
-std::expected<std::uint64_t, std::unique_ptr<Error>> read_build_id(const std::filesystem::path &path) {
+std::expected<std::uint64_t, std::unique_ptr<Error>> read_int_entry(const std::filesystem::path &path) {
     auto readfile_result = pms_utils::misc::try_readfile(path);
 
     if (!readfile_result) {
@@ -91,14 +95,36 @@ template <typename T> T unwrap(std::expected<T, std::unique_ptr<Error>> result, 
         result.error()->error(std::move(message));
     }
 
-    return result.value();
+    return *result;
+}
+
+std::expected<depend::DependExpr, std::unique_ptr<Error>> read_depend(const std::filesystem::path &path) {
+    auto readfile_result = pms_utils::misc::try_readfile(path);
+
+    if (!readfile_result) {
+        return std::unexpected(std::make_unique<IoError>(IoError(readfile_result.error())));
+    }
+
+    const auto &buffer = readfile_result.value();
+
+    auto parse_result = pms_utils::misc::try_parse(std::string_view(buffer.begin(), buffer.end()),
+                                                   pms_utils::parsers::depend::group, true);
+
+    if (!parse_result) {
+        return std::unexpected(std::make_unique<ParseError>(ParseError(std::string(parse_result.error()))));
+    }
+
+    return parse_result.value();
 }
 
 } // namespace
 // namespace
 
 Entry::Entry(const std::filesystem::path &path) {
-    _build_id = unwrap(read_build_id(path / "BUILD_ID"), "failed to load BUILD_ID");
+    _depend = unwrap(read_depend(path / "DEPEND"), "failed to load DEPEND");
+    _bdepend = unwrap(read_depend(path / "DEPEND"), "failed to load BDEPEND");
+    _rdepend = unwrap(read_depend(path / "DEPEND"), "failed to load RDEPEND");
+    _build_id = unwrap(read_int_entry(path / "BUILD_ID"), "failed to load BUILD_ID");
 };
 } // namespace pms_utils::vdb
 
